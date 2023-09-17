c9ac191db47de11722dd97f7d404b2e75403454e
mahin <mahinwaghray@gmail.com>
2023-09-16 21:04:46 -0400

    Intake for Ri3w first iteration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Intake.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
+import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestMotor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Intake implements Mechanism{
+ private DcMotor intakeLeft;
+ private DcMotor intakeRight;
+    @Override
+    public void init(HardwareMap hwMap) {
+        intakeRight = hwMap.get(DcMotor.class, "Intake_Right");
+        intakeLeft = hwMap.get(DcMotor.class, "Intake_Left");
+        intakeRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakeLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+    public void Run_intake(){
+        intakeRight.setPower(-1.0);
+        intakeLeft.setPower(1.0);
+    }
+    public void Stop_intake(){
+        intakeRight.setPower(0);
+        intakeLeft.setPower(0);
+    }
+    public void Eject_intake(){
+        intakeRight.setPower(0.5);
+        intakeLeft.setPower(-0.5);
+    }
+
+    @Override
+    public List<QQtest> getTests() {
+        return Arrays.asList(
+                new TestMotor("Intake Right", 0.2, intakeRight),
+                new TestMotor("Intake Left", 0.2, intakeLeft));
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/Ri3w.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.ftc16072.Mechanisms.Intake;
+import org.firstinspires.ftc.teamcode.ftc16072.Robot;
+@TeleOp
+public class Ri3w extends OpMode {
+    Robot robot = new Robot();
+    public void init() {
+  robot.init(hardwareMap);
+
+
+    }
+
+    @Override
+    public void loop() {
+
+
+    }
+}
