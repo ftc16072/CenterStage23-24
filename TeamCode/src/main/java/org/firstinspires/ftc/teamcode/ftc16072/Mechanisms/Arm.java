b93971e4cb0308e434cb00057b0bd27822265633
Joshua Smith <joshua@randomsmiths.com>
2023-09-23 08:45:53 -0400

    Adds controls to Arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Arm.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestMotor;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestServo;
@@ -12,56 +16,100 @@ import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestServo;
 import java.util.Arrays;
 import java.util.List;
 
+@Config
 public class Arm implements Mechanism{
+    static final double TICKS_PER_REV = 751.8; // From GoBilda for a 223 RPM
+    public static double WRIST_INTAKE_POS = 0.0;
+    public static double WRIST_PLACING_POS = 1.0;
+    public static int ARM_INTAKE_POS = 0;
+    public static int ARM_PLACING_ANGLE = 120;
+    public static double K_P = 0.01;
+    public static double K_I = 0;
+    public static double K_D = 0.2;
+    public static double K_F = 0.1;
+    public static double MAX_ARM_SPEED = 0.25;
+    public static int TOLERANCE_TICKS = 20;
+
+    public static double ARM_TICKS =
+            TICKS_PER_REV * ((double)ARM_PLACING_ANGLE / 360);
+    public static int ARM_PLACING_POS = (int) ARM_TICKS;
+
+    int desiredArmPosition;
     DcMotorEx arm;
     Servo wrist;
+    ElapsedTime timer = new ElapsedTime();
     @Override
     public void init(HardwareMap hwMap) {
         arm = hwMap.get(DcMotorEx.class, "arm");
         wrist = hwMap.get(Servo.class, "wrist");
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    public boolean goToTop(){
-
-        return goTo(40);
-    }
-
-    public boolean goToBottom(){
-        return false;
+    public void findServoLocation(){
+        wrist.setPosition(WRIST_INTAKE_POS);
     }
 
-    public boolean manualUp(){
-        return false;
+    public void goToPlacementPos(){
+        desiredArmPosition = ARM_PLACING_POS;
+        timer.reset();
     }
 
-    public boolean manualDown(){
-        return false;
+    public void goToIntakePos(){
+        desiredArmPosition = ARM_INTAKE_POS;
+        timer.reset();
     }
 
+    int lastError = 0;
+    double integralSum = 0;
+    public void update(Telemetry telemetry){
+        int armTicks = arm.getCurrentPosition();
+        //getDesiredWristPosition(armTicks, telemetry);
+        wrist.setPosition(getDesiredWristPosition(armTicks, telemetry));
 
-    public boolean goTo(int position){
-        moveMotor(position);
-        moveServo(position);
-        return true;
+        int error = desiredArmPosition - armTicks;
+        double derivative = (error - lastError) * timer.seconds();
+        integralSum = integralSum + (error * timer.seconds());
+        if(integralSum > 1){
+            integralSum = 1;
+        }
+        else if(integralSum < -1){
+            integralSum = -1;
+        }
 
+        double power = error * K_P + integralSum * K_I + derivative * K_D + K_F;
+        lastError = error;
+        timer.reset();
 
+        if(Math.abs(error) < TOLERANCE_TICKS){
+            power = 0;
+        }
+        power = Math.min(MAX_ARM_SPEED, Math.abs(power)) * Math.signum(power);
+        arm.setPower(power);
+        telemetry.addData("Arm power", power);
+        telemetry.addData("Desired Pos", desiredArmPosition);
+        telemetry.addData("Actual Pos", armTicks);
     }
-    public void moveMotor(int position){
-        arm.setTargetPosition(convertToMotorTicks(position));
-        wrist.setPosition(convertToServoPosition(position));
 
-    }
-    public void moveServo(int position){
+    public double getDesiredWristPosition(int armTicks, Telemetry telemetry){
+        double wristRange = WRIST_PLACING_POS - WRIST_INTAKE_POS;
+        int armRange  = ARM_PLACING_POS - ARM_INTAKE_POS;
 
+        int armOffset = armTicks - ARM_INTAKE_POS;
+        double wristOffset = armOffset * (wristRange / armRange);
+        telemetry.addData("Arm ticks", armTicks);
+        telemetry.addData("Arm Offset", armOffset);
+        telemetry.addData("Wrist Offset", wristOffset);
+        telemetry.addData("Wrist Pos", wristOffset + WRIST_INTAKE_POS);
 
+        double wristPos = wristOffset + WRIST_INTAKE_POS;
+        if(wristPos < WRIST_INTAKE_POS){
+            return WRIST_INTAKE_POS;
+        }else if (wristPos > WRIST_PLACING_POS){
+            return WRIST_PLACING_POS;
+        }
+        return wristPos;
     }
-    public int convertToMotorTicks(int angle){
-        return angle*223;
-    }
-    public int convertToServoPosition(int angle){
-        return angle/300;
-    }
-
 
     @Override
     public List<QQtest> getTests() {
@@ -69,6 +117,4 @@ public class Arm implements Mechanism{
                 new TestMotor("Arm", 0.2, arm),
                 new TestServo("Wrist", 0, 0.5, wrist));
     }
-
-
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/TestArm.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ftc16072.Robot;
+
+@TeleOp()
+public class TestArm extends OpMode {
+    Robot robot = new Robot();
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.left_bumper){
+            robot.arm.goToIntakePos();
+        }
+        else if(gamepad1.right_bumper){
+            robot.arm.goToPlacementPos();
+        }
+        else if(gamepad1.a) {
+            robot.arm.findServoLocation();
+        }
+        robot.arm.update(telemetry);
+    }
+}
