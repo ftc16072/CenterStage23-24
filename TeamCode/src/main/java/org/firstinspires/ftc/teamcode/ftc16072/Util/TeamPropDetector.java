539a3a75861a6c6e07e2d9c195999f2acab1a2c7
Alan Wong <alanwong9664@gmail.com>
2023-09-24 20:56:25 -0400

    vision working but needs some optimizations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Util/TeamPropDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Util/TeamPropDetector.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Util;
 
 import android.graphics.Canvas;
+import android.provider.ContactsContract;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
@@ -8,12 +9,15 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 public class TeamPropDetector implements VisionProcessor {
     //makes "detection zones" for each tape zone
-    public Rect leftTapeDetectionZone = new Rect(20,20,50,50);
-    public Rect middleTapeDetectionZone = new Rect(20,20,50,50);
-    public Rect rightTapeDetectionZone = new Rect(20,20,50,50);
+    int parkingZone = 1;
+    public Scalar rectangleColor = new Scalar(0, 255, 0);
+    public Rect leftTapeDetectionZone = new Rect(140,100,50,50);
+    public Rect middleTapeDetectionZone = new Rect(40,20,50,50);
+    public Rect rightTapeDetectionZone = new Rect(70,30,50,50);
 
     //submats are smaller portions of the frame that you can get values from
     Mat submat = new Mat();
@@ -28,18 +32,43 @@ public class TeamPropDetector implements VisionProcessor {
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
+        Imgproc.cvtColor(frame, hsvMat, Imgproc.COLOR_RGB2HSV);
+        Imgproc.rectangle(frame,leftTapeDetectionZone, rectangleColor);
+        Imgproc.rectangle(frame,middleTapeDetectionZone, rectangleColor);
+        Imgproc.rectangle(frame,rightTapeDetectionZone, rectangleColor);
+
+        double RectLeft =  getAvgSaturation(hsvMat, leftTapeDetectionZone);
+        double RectMiddle = getAvgSaturation(hsvMat, middleTapeDetectionZone);
+        double RectRight  = getAvgSaturation(hsvMat, rightTapeDetectionZone);
 
 
-        return null;
+        if((RectLeft>RectMiddle) && (RectLeft>RectRight)) {
+            parkingZone = 1;
+            return 1;
+        } else if ((RectMiddle>RectLeft) && (RectMiddle>RectRight)){
+            parkingZone = 2;
+            return 2;
+        }else if ((RectRight>RectMiddle) && (RectRight>RectLeft)){
+            parkingZone = 3;
+            return 3;
+        }
+        parkingZone = 1; // guess if it cant decide
+        return 1;
+
+
+
+    }
+    public int getParkingZone(){
+        return parkingZone;
     }
 
     //gets average hsv values from a specific submat created earlier
     //parameters are submat to be used and location of submat on frame
-    protected Scalar getAvgHSV(Mat input, Rect rect){
+    protected double getAvgSaturation(Mat input, Rect rect){
         //find color from submat
         submat = input.submat(rect);
         Scalar color = Core.mean(submat);
-        return color;
+        return color.val[1];
     }
 
     //scales rectangle to
