45430d589b93feef8a62bf2bd12407fa34c2c0fd
Swayam Shah <swayamshah244@gmail.com>
2023-10-28 18:36:20 -0400

    Initial Teleop behavior tree comment

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Trees/TeleopTree.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trees;
+
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+
+/*
+http://behaviortrees.ftcteams.com/
+
+=4
+| ->
+| | (IsEndGame)
+| | =2
+| | | ->
+| | | | ?
+| | | | | (IsLastThreeSeconds)
+| | | | | -> (IsDroneLocation)
+| | | | | | (IsDroneButtonPressed)
+| | | | (HaveNotLaunchedDrone)
+| | | | [LaunchDrone]
+
+| | | ->
+| | | | (IsClimbLocation)
+| | | | (IsClimbButtonPressed)
+| | | | [Climb]
+
+| ?
+| | ->
+| | | (HasLessThan2Pixels)
+| | | (IfIntakeButtonPressed)
+| | |[SpinIntakeMotor]
+| | [StopIntakeMotor]
+| =2
+| | ->
+| | | (Has1or2Pixels)
+| | | (IfExtendSlideButtonPressed)
+| | | [ExtendSlides]
+| | | =2
+| | | | ->
+| | | | | (IfLeftReleasePixelButtonPressed)
+| | | | | [ReleaseLeftPixel]
+| | | | ->
+| | | | | (IfRightReleasePixelButtonPressed)
+| | | | | [ReleaseRightPixel]
+
+
+| | ->
+| | | (AreSlidesNotExtended)
+| | | (IsControllerDriving)
+| | | [Drive]
+
+
+
+ */
+public class TeleopTree {
+
+
+}
