e7c811fd65b9510bb37c6b06592cef35b795646b
Alan Wong <alanwong9664@gmail.com>
2023-11-04 18:34:54 -0400

    fixed up mechanism code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Intake.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
+import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestColorRangeSensor;
+import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestColorSensor;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestMotor;
 
 import java.util.Arrays;
@@ -14,19 +16,25 @@ import java.util.List;
 public class Intake implements Mechanism{
 
     private DcMotor intakeMotor;
-    private ColorRangeSensor pixelCounter;
+    private ColorRangeSensor leftPixelCounter;
+    private ColorRangeSensor rightPixelCounter;
 
     @Override
     public void init(HardwareMap hwMap) {
         intakeMotor = hwMap.get(DcMotor.class, "Intake_Motor");
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftPixelCounter = hwMap.get(ColorRangeSensor.class, "left_pixel_counter");
+        rightPixelCounter = hwMap.get(ColorRangeSensor.class, "right_pixel_counter");
 
 
     }
 
     public List<QQtest> getTests() {
         return Arrays.asList(
-                new TestMotor("Intake Left", 0.2, intakeMotor));
+                new TestMotor("Intake Left", 0.2, intakeMotor),
+                new TestColorRangeSensor("left pixel detector", leftPixelCounter),
+                new TestColorRangeSensor("right pixel detector", rightPixelCounter));
+
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Lift.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
+import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestSwitch;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.TestTwoMotor;
 
 import java.util.Arrays;
@@ -14,10 +16,13 @@ import java.util.List;
 public class Lift implements  Mechanism{
     private DcMotorEx rightLiftMotor;
     private DcMotorEx leftLiftMotor;
+
+    private DigitalChannel liftLimitSwitch;
     @Override
     public void init(HardwareMap hwMap) {
         leftLiftMotor = hwMap.get(DcMotorEx.class, "left_lift_motor");
         rightLiftMotor = hwMap.get(DcMotorEx.class, "right_lift_motor");
+        liftLimitSwitch = hwMap.get(DigitalChannel.class,"lift_limit_switch");
         rightLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
@@ -26,7 +31,8 @@ public class Lift implements  Mechanism{
     @Override
     public List<QQtest> getTests() {
         return Arrays.asList(
-                new TestTwoMotor("lift",leftLiftMotor,rightLiftMotor,0.5)
+                new TestTwoMotor("lift",leftLiftMotor,rightLiftMotor,0.5),
+                new TestSwitch("lift limit switch",liftLimitSwitch)
 
         );
     }
