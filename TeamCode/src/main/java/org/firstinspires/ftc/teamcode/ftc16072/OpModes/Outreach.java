343127d7406b1273422acb22dd4be3a6176c9bc1
Swayam Shah <swayamshah244@gmail.com>
2023-11-04 14:23:48 -0400

    Made Outreach opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/Outreach.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class Outreach extends QQOpMode {
+    public static final double OUTREACH_SPEED = .5;
+    public static final double OUTREACH_TURBO_SPEED = .75;
+    public static final double MAX_SPEED = 1.0;
+    public static final double TRIGGER_THRESHOLD = 0.2;
+
+    @Override
+    public void init() {
+        robot.makeDriveOnly();
+        super.init();
+    }
+
+    @Override
+    public void loop() {
+        double speed_multiplier = OUTREACH_SPEED;
+        if(gamepad1.right_trigger > TRIGGER_THRESHOLD){
+            speed_multiplier = OUTREACH_TURBO_SPEED;
+            if(gamepad1.right_bumper && gamepad1.left_bumper){
+                speed_multiplier = MAX_SPEED;
+            }
+        }
+
+        robot.nav.fieldRelative(speed_multiplier * -gamepad1.left_stick_y,
+                speed_multiplier * gamepad1.left_stick_x,
+                speed_multiplier * gamepad1.right_stick_x);
+    }
+}
