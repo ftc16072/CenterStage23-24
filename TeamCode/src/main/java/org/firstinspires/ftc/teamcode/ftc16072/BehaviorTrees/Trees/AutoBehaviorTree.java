57f10c032cfed80d09ffa99f1fd5ef298056b01c
alanwong9664 <57013773+alanwong9664@users.noreply.github.com>
2023-12-02 18:45:47 -0500

    started working on autobehavior tree

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Trees/AutoBehaviorTree.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Trees/AutoBehaviorTree.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trees;
 
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Actions.SetLeftSpikeTrajectory;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Actions.SetMiddleSpikeTrajectory;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Conditions.IsLeftSpike;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Conditions.IsRightSpike;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Failover;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Sequence;
+
 public class AutoBehaviorTree {
 }/*
 ->
@@ -33,3 +41,44 @@ public class AutoBehaviorTree {
 |  [PlacePixel]
 |  [CycleandPark]
 */
+public class AutoTree{
+    public static Node root(){
+         new Sequence(
+                 new Failover(
+                         new Sequence(
+                                 new IsLeftSpike(),
+                                 new SetLeftSpikeTrajectory()
+                         ),
+                         new Sequence(
+                                 new IsRightSpike(),
+                                 new setRightSpikeTrajectory()
+                         ),
+                         new SetMiddleSpikeTrajectory
+
+                 ),
+                 new FollowTrajectory(),
+                 new Failover(
+                         new Sequence(
+                                 new SetBlueFarTrajectory(),
+                                 new IsRed(),
+                                 new SetRedFarTrajectory()
+                         )
+                         new Sequence(
+                                 new isClose(),
+                                 new Failover(
+                                         new SetBlueCloseTrajectory(),
+                                         new Sequence(
+                                                 new IsRed(),
+                                                 new SetRedCloseTrajectory()
+                                         )
+                                 )
+                         ),
+                        new SetBlueTrajectory()
+                         ),
+
+                 )
+
+         )
+
+    }
+}
