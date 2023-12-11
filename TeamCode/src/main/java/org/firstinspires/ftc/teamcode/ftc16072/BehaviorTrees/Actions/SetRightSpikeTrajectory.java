1e1fd9345a591465fdc46fc5af10cf349777bddf
Alan Wong <alanwong9664@gmail.com>
2023-12-11 16:04:07 -0500

    added tape parking trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Actions/SetRightSpikeTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Actions/SetRightSpikeTrajectory.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Actions;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
 import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
 import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
 import org.firstinspires.ftc.teamcode.rr_trajectorysequence.TrajectorySequence;
@@ -12,13 +15,15 @@ public class SetRightSpikeTrajectory extends Node {
     @Override
     public State tick(QQOpMode opMode) {
         TrajectorySequenceBuilder builder = opMode.robot.nav.trajectorySequenceBuilder(opMode.robot.nav.getPoseEstimate());
-        TrajectorySequence sequence = builder.forward(FORWARD_DISTANCE_INCHES).
-                                            strafeRight(RIGHT_DISTANCE_INCHES).
-                                            build();
-        if (sequence == null){
+        TrajectorySequence rightParking = opMode.robot.nav.trajectorySequenceBuilder(new Pose2d(-36.90, -59.86, Math.toRadians(90.00)))
+                .splineTo(new Vector2d(-30.01, -39.03), Math.toRadians(44.05))
+                .build();
+
+
+        if (rightParking == null){
             return State.FAILURE;
         }
-        opMode.board.add("TrajectorySequence", sequence);
+        opMode.board.add("TrajectorySequence", rightParking);
         return State.SUCCESS;
     }
 }
