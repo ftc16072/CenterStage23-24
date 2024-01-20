5f0a7f24aedf8d9080f8c7f2d944a8b299dfa44b
arnavchauhan <alanwong9664@gmail.com>
2024-01-20 13:02:14 -0500

    Working on adding vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/AprilTag.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Locale;
 
 @TeleOp
-
 public class AprilTag extends VisionBase {
 
     @Override
@@ -17,7 +16,8 @@ public class AprilTag extends VisionBase {
         super.init();
     }
     public void init_loop(){
-
+        telemetry.addLine("Front Camera");
+        printAprilTags(robot.cameraFront.getAprilTagDetections());
         telemetry.addLine("Back Camera");
         printAprilTags(robot.cameraBack.getAprilTagDetections());
         telemetry.addLine("------------");
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/RRTests/PlacingOnTapeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/RRTests/PlacingOnTapeAuto.java
@@ -5,24 +5,19 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
-import org.firstinspires.ftc.teamcode.ftc16072.OpModes.VisionBase;
 import org.firstinspires.ftc.teamcode.ftc16072.Robot;
 import org.firstinspires.ftc.teamcode.ftc16072.Util.TeamPropLocation;
 
 @Autonomous
-public class PlacingOnTapeAuto extends VisionBase {
+public class PlacingOnTapeAuto extends OpMode {
     Robot robot = new Robot();
     Trajectory trajectory;
     private enum State {BEGIN, FORWARD,TURN,FORWARD_TO_TAPE, DONE}
     State state = State.BEGIN;
     @Override
     public void init() {
+        robot.makeDriveOnly();
         robot.init(hardwareMap);
-        super.init(); // for vision
-    }
-    public void init_loop(){
-
     }
 
     @Override
@@ -31,7 +26,7 @@ public class PlacingOnTapeAuto extends VisionBase {
         Pose2d currentPose = robot.nav.getPoseEstimate();
         TeamPropLocation teamPropLocation = robot.cameraBack.getTeamPropPosition();
 
-        telemetry.addData("team prop", teamPropLocation);
+        telemetry.addData("STATE", state);
         telemetry.addData("POSE", "x = %.2f y = %.2f h = %.1f", currentPose.getX(), currentPose.getY(), Math.toDegrees(currentPose.getHeading()));
         switch (state) {
             case BEGIN:
@@ -56,56 +51,38 @@ public class PlacingOnTapeAuto extends VisionBase {
                 robot.nav.follower.followTrajectory(trajectory);
                 break;
             case FORWARD:
-
-                if (robot.nav.isDoneFollowing(currentPose)) {
-                    if (teamPropLocation == TeamPropLocation.LEFT_SPIKE){
-                        if (robot.nav.isDoneFollowing(currentPose)) {
-                            robot.nav.turnAsync(-0.25*Math.PI);
-                        }
-                    } else if (teamPropLocation == TeamPropLocation.RIGHT_SPIKE) {
-                        if (robot.nav.isDoneFollowing(currentPose)) {
-                            robot.nav.turnAsync(0.25*Math.PI);
-                        }
-
+                state = State.TURN;
+                if (teamPropLocation == TeamPropLocation.LEFT_SPIKE){
+                    if (robot.nav.isDoneFollowing(currentPose)) {
+                        robot.nav.turnAsync(-0.25*Math.PI);
                     }
-                    state = State.TURN;
+                } else if (teamPropLocation == TeamPropLocation.RIGHT_SPIKE) {
+                    if (robot.nav.isDoneFollowing(currentPose)) {
+                        robot.nav.turnAsync(0.25*Math.PI);
+                    }
 
                 }
-
-
 
                 break;
 
 
             case TURN:
                 state = State.FORWARD_TO_TAPE;
-                if (robot.nav.isDoneFollowing(currentPose)) {
+                if (teamPropLocation == TeamPropLocation.LEFT_SPIKE){
+                    trajectory = robot.nav.trajectoryBuilder(currentPose, false)
+                            .back(12)
+                            .build();
 
 
-                    if (teamPropLocation == TeamPropLocation.LEFT_SPIKE) {
-                        trajectory = robot.nav.trajectoryBuilder(currentPose, false)
-                                .back(7)
-                                .build();
-
-
-                    } else if (teamPropLocation == TeamPropLocation.RIGHT_SPIKE) {
-                        trajectory = robot.nav.trajectoryBuilder(currentPose, false)
-                                .back(7)
-                                .build();
-                    }
+                } else  if (teamPropLocation == TeamPropLocation.RIGHT_SPIKE){
+                    trajectory = robot.nav.trajectoryBuilder(currentPose, false)
+                            .back(12)
+                            .build();
                 }
                 break;
             case FORWARD_TO_TAPE:
-                if (robot.nav.isDoneFollowing(currentPose)) {
-                    state = State.DONE;
-                }
-
-
-
+                state = State.DONE;
                 break;
-            case DONE:
-                break;
-
         }
 
 
