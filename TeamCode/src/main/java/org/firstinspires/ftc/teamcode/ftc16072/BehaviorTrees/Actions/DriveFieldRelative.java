ba5582f8f5b1ea721ed97f88dc73db3e84755099
mahin <mahinwaghray@gmail.com>
2024-01-22 20:11:38 -0500

    Turned Turbo into Slow Mode and square with sign

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Actions/DriveFieldRelative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Actions/DriveFieldRelative.java
@@ -4,12 +4,15 @@ import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
 import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
 
 public class DriveFieldRelative extends Node {
+   private double squareWithSign(double number){
+       return number * number * Math.signum(number);
+   }
 
     @Override
     public State tick(QQOpMode opmode) {
-        opmode.robot.nav.fieldRelative(-opmode.gamepad1.left_stick_y,
-                opmode.gamepad1.left_stick_x,
-                opmode.gamepad1.right_stick_x);
+        opmode.robot.nav.fieldRelative(squareWithSign(-opmode.gamepad1.left_stick_y),
+                squareWithSign(opmode.gamepad1.left_stick_x),
+                squareWithSign(opmode.gamepad1.right_stick_x));
         return State.RUNNING;
     }
 }
