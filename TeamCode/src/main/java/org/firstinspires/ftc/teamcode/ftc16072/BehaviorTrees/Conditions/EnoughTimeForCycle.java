0c2589cc0a9aa7029607d038935c92a59f61e5f1
alanwong9664 <57013773+alanwong9664@users.noreply.github.com>
2024-01-27 18:52:00 -0500

    started auto behavior tree

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Conditions/EnoughTimeForCycle.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Conditions;
+
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
+
+public class EnoughTimeForCycle extends Node{
+        public static final int AUTO_TIME = //;
+
+        @Override
+        public State tick(QQOpMode opmode) {
+            if(opmode.time > CYCLETIME){
+                return State.SUCCESS;
+            }
+            return State.FAILURE;
+        }
+    }
+
