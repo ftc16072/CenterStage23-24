5c10e5427660a18f54e8e409a19b069aeedf17d2
alanwong9664 <57013773+alanwong9664@users.noreply.github.com>
2024-01-26 20:27:02 -0500

    fixed build issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Robot.java
@@ -41,7 +41,7 @@ public class Robot {
         cameraBack = new Camera(Camera.CameraPosition.BACK);
         controlHub = new ControlHub();
         mecanumDrive = new MecanumDrive();
-        nav = new Navigation(controlHub, mecanumDrive, cameraFront,cameraBack);
+        nav = new Navigation(controlHub, mecanumDrive, cameraBack);
         intake = new Intake();
         holdingCell = new HoldingCell();
         lift = new Lift();
