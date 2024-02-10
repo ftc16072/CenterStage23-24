466eeaa6da7dded26ea4cae61dc246c7fbf8227a
arnavchauhan <alanwong9664@gmail.com>
2024-02-10 15:32:55 -0500

    Adjust backbaord range

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Conditions/IsBackboardInRange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Conditions/IsBackboardInRange.java
@@ -4,7 +4,7 @@ import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
 import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
 
 public class IsBackboardInRange extends Node {
-    public double BACKBOARD_RANGE_CM = 10;
+    public double BACKBOARD_RANGE_CM = 15;
     @Override
     public State tick(QQOpMode opmode) {
         if (opmode.robot.backDistanceSensor.distanceToBackboard()<BACKBOARD_RANGE_CM){
