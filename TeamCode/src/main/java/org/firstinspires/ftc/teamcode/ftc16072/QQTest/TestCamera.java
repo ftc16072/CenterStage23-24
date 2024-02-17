10cddd5978cc98a3525432031605636f591c1cf0
arnavchauhan <alanwong9664@gmail.com>
2024-02-17 16:19:16 -0500

    Added spike loction in test wiring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/QQTest/TestCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/QQTest/TestCamera.java
@@ -2,16 +2,21 @@ package org.firstinspires.ftc.teamcode.ftc16072.QQTest;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.ftc16072.Util.TeamPropDetector;
 
 public class TestCamera extends QQtest{
+    TeamPropDetector teamPropDetector;
     WebcamName webcamName;
 
-    public TestCamera(String name, WebcamName webcamName){
+    public TestCamera(String name, WebcamName webcamName, TeamPropDetector teamPropDetector){
         super(name);
         this.webcamName = webcamName;
+        this.teamPropDetector = teamPropDetector;
     }
     @Override
     public void run(boolean on, Telemetry telemetry) {
         telemetry.addData("Is Camera Attached", webcamName.isAttached());
+        telemetry.addData("teamProplocation: ", teamPropDetector.getPropLocation());
+
     }
 }
