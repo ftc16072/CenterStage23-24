7a5f5262d734ad985973f1815640b6c2a2c7e8b6
arnavchauhan <alanwong9664@gmail.com>
2024-02-16 19:31:09 -0500

    Implemented spike vision for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/BlueClose_SpikeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/BlueClose_SpikeAuto.java
@@ -2,14 +2,21 @@ package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trees.SpikeAutoTree;
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trees.TeleopTree;
 import org.firstinspires.ftc.teamcode.ftc16072.Util.Alliance;
 import org.firstinspires.ftc.teamcode.ftc16072.Util.StartPosition;
 @Autonomous
 public class BlueClose_SpikeAuto extends SpikeAutoBase {
+
+
+
     @Override
     public void init(){
         board.add("Alliance", Alliance.BLUE);
         board.add("StartPosition", StartPosition.CLOSE);
         super.init();
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/SpikeAutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/SpikeAutoBase.java
@@ -4,12 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
 import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trees.SpikeAutoTree;
+import org.firstinspires.ftc.teamcode.ftc16072.Util.Alliance;
 
 abstract public class SpikeAutoBase extends VisionBase{
     Node root = SpikeAutoTree.root();
+
     boolean done;
     @Override
     public void init(){
+
+        board.add("proploocation", robot.cameraBack.getTeamPropPosition());
         robot.makeDriveOnly();
         super.init();
     }
