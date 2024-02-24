c825411c61f2f620884fc374dd0640d155b054cd
arnavchauhan <alanwong9664@gmail.com>
2024-02-24 06:37:46 -0500

    add BC2Middle Trajectory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Trajectories/BlueClose/MiddleSpike/BC2MiddleSpikeTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Trajectories/BlueClose/MiddleSpike/BC2MiddleSpikeTrajectory.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Trajectories.BlueClose.MiddleSpike;
 
-public class BC2MiddleSpikeTrajectory {
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
+import org.firstinspires.ftc.teamcode.ftc16072.Util.SpikePosition;
+import org.firstinspires.ftc.teamcode.rr_trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.rr_trajectorysequence.TrajectorySequenceBuilder;
+
+public class BC2MiddleSpikeTrajectory extends Node {
+    public static final int RIGHT_DISTANCE_INCHES = 38;
+
+
+    @Override
+    public State tick(QQOpMode opMode) {
+        TrajectorySequenceBuilder builder = opMode.robot.nav.trajectorySequenceBuilder(opMode.robot.nav.getPoseEstimate());
+        TrajectorySequence sequence = builder
+                .strafeRight(RIGHT_DISTANCE_INCHES).
+                build();
+        if (sequence == null){
+            return State.FAILURE;
+        }
+        opMode.board.add("TrajectorySequence", sequence);
+        opMode.board.add("SpikePosition", SpikePosition.MIDDLE);
+        return State.SUCCESS;
+    }
 }
