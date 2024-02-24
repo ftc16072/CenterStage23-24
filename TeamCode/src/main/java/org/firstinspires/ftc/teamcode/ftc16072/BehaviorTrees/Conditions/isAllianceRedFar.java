f6411a15305b5ff02080393f246ac5558a5f6073
arnavchauhan <alanwong9664@gmail.com>
2024-02-23 23:55:12 -0500

    node to check for alliance location

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/BehaviorTrees/Conditions/isAllianceRedFar.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Conditions;
+
+import org.firstinspires.ftc.teamcode.ftc16072.BehaviorTrees.Node;
+import org.firstinspires.ftc.teamcode.ftc16072.OpModes.QQOpMode;
+import org.firstinspires.ftc.teamcode.ftc16072.Util.Alliance;
+import org.firstinspires.ftc.teamcode.ftc16072.Util.StartPosition;
+
+public class isAllianceRedFar extends Node {
+    public State tick(QQOpMode opmode) {
+        Alliance alliance = opmode.board.get(Alliance.class, "Alliance");
+        StartPosition startPosition = opmode.board.get(StartPosition.class,"StartPosition");
+
+        if (alliance == Alliance.RED || startPosition == StartPosition.FAR ){
+            return State.SUCCESS;
+        }
+        return State.FAILURE;
+    }
+}
